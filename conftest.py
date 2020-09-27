89461227d95626d3b91dc7c559ac086fab7673af
AkZushka <azavisnov@yandex.ru>
2020-09-27 11:08:45 +0300

    obnovlenie testa i configa

--- conftest.py
+++ conftest.py
@@ -1,18 +1,25 @@
 import pytest
+import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    user_language = request.config.getoption("language")
     browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': user_language})  
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
@@ -21,3 +28,4 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
+
--- test_items.py
+++ test_items.py
@@ -1,9 +1,11 @@
-link = "http://selenium1py.pythonanywhere.com/" 
+import time
+from selenium import webdriver
+
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
 
 def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("#login_link")
-
-def test_guest_should_see_login_link_fail(browser):
-    browser.get(link)
-    browser.find_element_by_css_selector("#magic_link")
+    time.sleep(10) #визуальная проверка
+    x = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    but = len(x)
+    assert but > 0, "Not Found!!!"
