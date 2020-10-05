b069a416556ba748a8e3b31c92afcd3cdd03768f
AkZushka <azavisnov@yandex.ru>
2020-10-05 10:09:31 +0300

    Peredelal selector na bolee unicalniy

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_basket_button(browser):
     browser.get(link)
     time.sleep(10) #визуальная проверка
-    x = browser.find_elements_by_css_selector(".btn-add-to-basket")
+    x = browser.find_elements_by_css_selector(".add-to-basket .btn-primary")
     but = len(x)
     assert but > 0, "Not Found!!!"
